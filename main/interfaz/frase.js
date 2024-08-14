e8b5780bd831560983e86b38be121381c551a3a1
mariposa <mardevoir@proton.me>
2024-08-14 03:08:37 +0200

    Update frase.js

--- main/interfaz/frase.js
+++ main/interfaz/frase.js
@@ -1,6 +1,6 @@
 async function cargarFrases() {
     try {
-        const response = await fetch('/main/frases.txt');
+        const response = await fetch('/frases.txt');
         const data = await response.text();
         const frases = data.split('\n').filter(frase => frase.trim() !== "");
         const fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
